10b799f5602e4e56ca7b9f59dcb0588a9aa03408
Michael Melesse <micmelesse@gmail.com>
2020-01-13 11:03:28 -0800

    force bash

--- scripts/rocblas_bench.sh
+++ scripts/rocblas_bench.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# use bash
+SCRIPT_PATH=$(realpath $0)
+if [ ! "$BASH_VERSION" ]; then
+    echo "Using bash to run this script $0" 1>&2
+    exec bash $SCRIPT_PATH "$@"
+    exit 1
+fi
+
 export HIP_VISIBLE_DEVICES=0 # choose gpu
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/dockerx/rocBLAS/blis/lib
 DATA_DIR=$(pwd)/data
@@ -7,5 +16,4 @@ cd /opt/rocm/rocblas/bin # switch to rocblas-bench directory
 while IFS= read -r line; do
     FILTERED_LINE=$(echo $line | grep -oP "./rocblas-bench.*\d")
     source <(echo $FILTERED_LINE)
-done <"$DATA_DIR/rocblas_log_bench_bert_512_hist.csv" > "$DATA_DIR/rocblas_log_bench_bert_512_hist_rbench_outpout.txt"
-
+done <"$DATA_DIR/rocblas_log_bench_bert_512_hist.csv" >"$DATA_DIR/rocblas_log_bench_bert_512_hist_rbench_outpout.txt"
