f0a67e0e6ce9053e93c53863b81ffd9ad194013e
Michael Melesse <mmelesse@ixt-rack-98.local.lan>
2020-02-21 20:45:51 -0500

    add set dpm scripts

---
+++ scripts/set_dpm4.sh
@@ -0,0 +1,2 @@
+/opt/rocm/bin/rocm-smi --setsclk 4
+/opt/rocm/bin/rocm-smi -a >data/dpm4_status.txt
---
+++ scripts/set_dpm8.sh
@@ -0,0 +1,2 @@
+/opt/rocm/bin/rocm-smi --setsclk 8
+/opt/rocm/bin/rocm-smi -a >data/dpm8_status.txt
