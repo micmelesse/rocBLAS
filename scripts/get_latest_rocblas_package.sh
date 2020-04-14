944eccfbc938dad5d273cd0f90772bfe7d655e8d
Michael Melesse <mmelesse@ixt-rack-98.local.lan>
2020-04-14 15:27:29 -0400

    update scripts

--- scripts/get_latest_rocblas_package.sh
+++ scripts/get_latest_rocblas_package.sh
@@ -14,10 +14,10 @@ mkdir -p rocblas_package_latest
 cd rocblas_package_latest
 
 #download and unzip packages
-wget -nc http://math-ci.rocm.amd.com/job/rocm-docker/job/extended/job/rocBLAS/job/develop/lastSuccessfulBuild/artifact/XCPStu8SH/rocblas/build/release/package/*zip*/package.zip
+wget -nc http://math-ci.rocm.amd.com/job/rocm-docker/job/extended/job/rocBLAS/job/develop/lastSuccessfulBuild/
 unzip -n package.zip
 
 
-# install packages
-PKG_DIR=package
-dpkg -i $PKG_DIR/rocblas-*ubuntu-16.04*.deb
+# # install packages
+# PKG_DIR=package
+# dpkg -i $PKG_DIR/rocblas-*ubuntu-18.04*.deb
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -21,7 +21,10 @@ PKG_DIR=rocblas_packages
 
 # install packages
 # dpkg -i $PKG_DIR/rocblas-2.*ubuntu*.deb
-dpkg -i $PKG_DIR/bert-mi60-tuned.deb
-dpkg -i $PKG_DIR/rocblas-clients*.deb
+# dpkg -i $PKG_DIR/bert-mi60-tuned.deb
+# dpkg -i rocblas_packages/rocblas-clients-2.15.0.1853-develop-20-eb4da81-Linux.deb
+# dpkg -i rocblas_packages/rocblas-clients-2.19.0.2040-develop-18-a8a065b-ubuntu-16.04_amd64.deb
+# dpkg -i rocblas_packages/rocblas-clients-2.21.0.2258-develop-37-a7a2fdd8-ubuntu-18.04_amd64.deb
+dpkg -i rocblas_packages/rocblas-clients-2.19.0.2072-develop-28-6a1fc5a6-ubuntu-18.04_amd64.deb
 
 # cp -r /var/jenkins/workspace/Extended_rocBLAS_develop/*/rocblas/build/release/rocblas-install/rocblas/bin /opt/rocm/rocblas
